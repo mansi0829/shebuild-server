9f455d0215b3236bf515cf80fda40698a1a69151
mansi0829 <mansigundre1@gmail.com>
2022-12-29 21:06:26 +0530

    setup

---
+++ src/middlewares/JWT.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const JWT = require("jsonwebtoken");
+
+async function generateToken(user) {
+  const token = await JWT.sign(
+    {
+      id: user._id,
+    },
+    process.env.Secret,
+    {
+      expiresIn: 6000000,
+    }
+  );
+  return token;
+}
+
+async function checkToken(req, res, next) {
+  const token = req.headers["authorization"];
+  console.log(token);
+  if (!token) {
+    return res.status(400).json({
+      msg: "No token found",
+    });
+  }
+
+  try {
+    const user = JWT.verify(token, process.env.Secret);
+    req.body.userID = user.id;
+  } catch (error) {
+    return res.status(400).json({
+      msg: "Token Invalid",
+    });
+  }
+  return next();
+}
+
+module.exports = { generateToken, checkToken };
